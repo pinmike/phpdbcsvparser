5f93b827ed510ed40715778e462fdd9e6b56242d
Michael Pinnell <mike.pinnell@gmail.com>
2014-10-15 17:25:47 +0100

    added comments

--- process.php
+++ process.php
@@ -1,39 +1,47 @@
 <?php
 
-$inputFileName = 'exampleinput.csv';
-$outputFileName = 'exampleOutput.csv';
+/** Simple script to take a csv file in one form and output it in another */
 
-$inputFile = fopen($inputFileName, 'r');
-$outputFile = fopen($outputFileName, 'a');
+$inputFileName		= 'exampleinput.csv';
+$outputFileName 	= 'exampleOutput.csv';
+$dates 				= array();
+$indexes 			= array();
+$processData 		= array();
+$outputHeaderLine 	= "";
+$outputLine 		= "";
 
-$dates = array();
-$indexes = array();
-$processData = array();
-$outputHeaderLine = "";
-$outputLine = "";
+// Get handles on the in and out files
+$inputFile 			= fopen($inputFileName, 'r');
+$outputFile 		= fopen($outputFileName, 'a');
 
+// Iterate over each csv file line, parsing data in to new array
 while(($line = fgetcsv($inputFile)) !== FALSE)
 {
-	$index = $line[0];
-	$date = $line[1];
-	$value = $line[2];
+	$index 	= $line[0];
+	$date 	= $line[1];
+	$value 	= $line[2];
 
+	// Add to new array
 	$processData[$date][$index] = $value;
 
+	// Maintain an array of known date values
 	if(!in_array($date, $dates))
 	{
 		$dates[] = $date;
 	}
 
+	// Maintain an array of known index values
 	if(!in_array($index, $indexes))
 	{
 		$indexes[] = $index;
 	}
 }
 
+// Create and output the csv header line
 $outputHeaderLine = "date,".implode(',', $indexes)."\n";
 fwrite($outputFile, $outputHeaderLine);
 
+// Iterate over new array and write csv lines to file
 foreach($processData as $date => $dateData)
 {
 	$outputLine = $date;
@@ -52,5 +60,6 @@ foreach($processData as $date => $dateData)
 	fwrite($outputFile, $outputLine."\n");
 }
 
+// Close file handles
 fclose($inputFile);
 fclose($outputFile);
